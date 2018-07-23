1d5dd7863e596a83b30bf16ca314d86438480995
Guest User <Guest@epicodus-26.local>
2018-07-23 10:18:37 -0700

    Added initial pages

---
+++ HairSalon.Tests/ModelsTests/HairSalonTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon;
+
+namespace HairSalon.Tests
+{
+  [TestClass]
+  public class HairSalonTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
---
+++ HairSalon/Models/HairSalon.cs
@@ -0,0 +1,12 @@
+namespace HairSalon
+{
+public class HairSalon
+  {
+  public bool ExampleTest(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
+  
