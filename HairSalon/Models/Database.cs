a2d54bf6f59733ddbc2a9bc2e2e9ee9bf81281a6
Guest User <Guest@epicodus-26.local>
2018-07-23 10:28:51 -0700

    add Startup, Program, Models, Views Files

---
+++ HairSalon/Models/Database.cs
@@ -0,0 +1,15 @@
+using System;
+using MySql.Data.MySqlClient;
+using Airport;
+
+namespace Airport.Models
+{
+  public class DB
+  {
+    public static MySqlConnection Connection()
+    {
+      MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+      return conn;
+    }
+  }
+}
