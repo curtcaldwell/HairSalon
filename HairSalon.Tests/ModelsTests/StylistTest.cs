e40b441b2603521d5a0f6afa3b731848b8b4cf40
Guest User <Guest@epicodus-26.local>
2018-07-23 11:35:59 -0700

    create first test and fail

---
+++ HairSalon.Tests/ModelsTests/StylistTest.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using HairSalon.Models;
+
+namespace HairSalon.Tests
+{
+  [TestClass]
+  public class StylistTests : IDisposable
+  {
+    public StylistTests()
+    {
+        DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=Hair_Salon_Tests;";
+    }
+    public void Dispose()
+    {
+      Stylist.DeleteAll();
+    }
+    [TestMethod]
+    public void Save_Test()
+    {
+      //Arrange
+      Stylist testStylist = new Stylist("Mark");
+      testStylist.Save();
+      //Act
+      List<Stylist> testList = new List<Stylist>{testStylist};
+      List<Stylist> result = Stylist.GetAll();
+      //Assert
+      CollectionAssert.AreEqual(testList, result);
+}
+}
+}
